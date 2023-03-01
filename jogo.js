78dadfe273eca33e2189a9f438ea2eb2a49daf8c
torressg <126213815+torressg@users.noreply.github.com>
2023-03-01 16:45:58 -0300

    Add files via upload

--- jogo.js
+++ jogo.js
@@ -45,61 +45,6 @@ document.querySelector("#btnOpcao1_1").addEventListener("click", function() {
     document.querySelector("#opcao4").style.display = "block";
 });
 
-/*/document.querySelector("#btnOpcao1_2").addEventListener("click", function() {
-	alert("Simbora pra KEZ Padaria comer um Cinnamol Roll.\nCompra as passagens do metro, pega um guarda-chuva e partiu!");
-});/*/
-
-/*/document.querySelector("#btnOpcao1_3").addEventListener("click", function valorMotel() {
-});
-
-document.querySelector("#btnOpcao2_1").addEventListener("click", function() {
-	alert("Vamos para o parque dar um rolezin. Como é algo curto, você tem direito a mais uma rodada.");
-});
-
-document.querySelector("#btnOpcao2_2").addEventListener("click", function() {
-	alert("Vamos andar na rua, sem rumo, até achar algo que queira parar. Como é algo curto, você tem direito a mais uma rodada.");
-});
-
-document.querySelector("#btnOpcao2_3").addEventListener("click", function() {
-	alert("Vamos descer no shopping sem destino, e comer alguma coisinha, a nossa escolha.");
-});/*/
-
-/*/document.querySelector("#btnOpcao3_1").addEventListener("click", function() {
-	alert("BVoc� escolheu em ver um filme, mas nada aqui � normal, ent�o tinha chance de cair: MELHORAR E FAZER RANDOMICO");
-});/*/
-
-/*/document.querySelector("#btnOpcao3_2").addEventListener("click", function() {
-	alert("Voc� escolheu fazer uma receita!! Olha o que poderia cair: MELHORAR E FAZER RANDOMICO");
-});/*/
-
-/*/document.querySelector("#btnOpcao3_3").addEventListener("click", function() {
-	alert("Voc� vai pedir comida!! Entre no programa de Comidas.");
-});/*/
-
-/*/document.querySelector("#btnOpcao4_1").addEventListener("click", function() {
-	alert("Vamos para o Cinema do Atrium!\nQual filme veremos?");
-	document.querySelector("#opcao1").style.display = "none";
-	document.querySelector("#opcao2").style.display = "none";
-	document.querySelector("#opcao3").style.display = "none";
-    document.querySelector("#opcao4").style.display = "none";
-	document.querySelector("#opcaotit").style.display = "none";/*/
-
-
-/*/document.querySelector("#btnOpcao4_3").addEventListener("click", function() {
-	alert("Vamos para o Cinema do Plaza!\nQual filme veremos?");
-});
-
-document.querySelector("#btnOpcao4_2").addEventListener("click", function() {
-	alert("Vamos para o Cinema de SCS!\nQual filme veremos?");
-});/*/
-
-/*?document.querySelector("replay").addEventListener("click", function() {
-	document.querySelector("#opcao1").style.display = "none";
-	document.querySelector("#opcao2").style.display = "none";
-	document.querySelector("#opcao3").style.display = "none";
-    document.querySelector("#opcao4").style.display = "none";
-	document.querySelector("#opcaotit").style.display = "none";
-});/*/
 
 /*/Modal comeca aqui/*/
 
@@ -109,54 +54,97 @@ var btn = document.getElementById("btnOpcao4_1");
 
 var span = document.getElementsByClassName("close")[0];
 
-var modal = document.getElementById("myModal1");
-
-var btn = document.getElementById("btnOpcao4_2");
-
-var span = document.getElementsByClassName("close")[0];
-
-var modal = document.getElementById("myModal2");
-
-var btn = document.getElementById("btnOpcao4_3");
-
-var span = document.getElementsByClassName("close")[0];
-
-var modal = document.getElementById("myModal3");
-
-var btn = document.getElementById("btnOpcao1_2");
-
-var span = document.getElementsByClassName("close")[0];
-
-var modal = document.getElementById("myModal4");
-
-var btn = document.getElementById("btnOpcao2_1");
-
-var span = document.getElementsByClassName("close")[0];
-
-var modal = document.getElementById("myModal4");
-
-var btn = document.getElementById("btnOpcao2_1");
-
-var span = document.getElementsByClassName("close")[0];
-
-var modal = document.getElementById("myModal5");
-
-var btn = document.getElementById("btnOpcao2_2");
-
-var span = document.getElementsByClassName("close")[0];
-
-var modal = document.getElementById("myModal6");
-
-var btn = document.getElementById("btnOpcao2_3");
-
-var span = document.getElementsByClassName("close")[0];
-
 btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+	modal.style.display = "block";
   }
-}
+  
+  window.onclick = function(event) {
+	if (event.target == modal) {
+	  modal.style.display = "none";
+	}
+  }
+  
+var modal1 = document.getElementById("myModal1");
+
+var btn1 = document.getElementById("btnOpcao4_2");
+
+btn1.onclick = function() {
+	modal1.style.display = "block";
+  }
+  
+  window.onclick = function(event1) {
+	if (event1.target == modal1) {
+	  modal1.style.display = "none";
+	}
+  }
+
+var modal2 = document.getElementById("myModal2");
+
+var btn2 = document.getElementById("btnOpcao4_3");
+
+btn2.onclick = function() {
+	modal2.style.display = "block";
+  }
+  
+  window.onclick = function(event2) {
+	if (event2.target == modal2) {
+	  modal2.style.display = "none";
+	}
+  }
+
+var modal3 = document.getElementById("myModal3");
+
+var btn3 = document.getElementById("btnOpcao1_2");
+
+btn3.onclick = function() {
+	modal3.style.display = "block";
+  }
+  
+  window.onclick = function(event3) {
+	if (event3.target == modal3) {
+	  modal3.style.display = "none";
+	}
+  }
+
+var modal4 = document.getElementById("myModal4");
+
+var btn4 = document.getElementById("btnOpcao2_1");
+
+btn4.onclick = function() {
+	modal4.style.display = "block";
+  }
+  
+  window.onclick = function(event4) {
+	if (event4.target == modal4) {
+	  modal4.style.display = "none";
+	}
+  }
+
+var modal5 = document.getElementById("myModal5");
+
+var btn5 = document.getElementById("btnOpcao2_2");
+
+btn5.onclick = function() {
+	modal5.style.display = "block";
+  }
+  
+  window.onclick = function(event5) {
+	if (event5.target == modal5) {
+	  modal5.style.display = "none";
+	}
+  }
+
+var modal6 = document.getElementById("myModal6");
+
+var btn6 = document.getElementById("btnOpcao2_3");
+
+btn6.onclick = function() {
+	modal6.style.display = "block";
+  }
+  
+  window.onclick = function(event6) {
+	if (event6.target == modal6) {
+	  modal6.style.display = "none";
+	}
+  }
+
